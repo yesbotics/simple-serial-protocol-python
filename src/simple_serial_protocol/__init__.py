cfe452d02a498ca71e8252f8425d746dea1167bf
Albrecht Nitsche <albrecht.nitsche@gmail.com>
2024-05-13 12:33:00 +0200

    expose ParamTypes

--- src/simple_serial_protocol/__init__.py
+++ src/simple_serial_protocol/__init__.py
@@ -1,4 +1,5 @@
 from .SimpleSerialProtocol import SimpleSerialProtocol
 from .SimpleSerialProtocol import CommandParam
 from .serial_port import *
+from .param_type import *
 from .Baudrate import Baudrate
