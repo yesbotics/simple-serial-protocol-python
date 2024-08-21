297e5ff83bccd1a73a333ffc4ebcb353bb7c01a1
Albrecht Nitsche <albrecht.nitsche@gmail.com>
2024-08-21 13:57:37 +0200

    refact: does not mangle property name __raw_data

--- src/simple_serial_protocol/param_type/ParamTypeBoolean.py
+++ src/simple_serial_protocol/param_type/ParamTypeBoolean.py
@@ -10,8 +10,8 @@ class ParamTypeBoolean(ParamType[bool]):
 
     def __init__(self) -> None:
         super().__init__()
-        self.__raw_data: bytearray = bytearray(self.get_length())
-        self.__index: int | None = None
+        self._raw_data: bytearray = bytearray(self.get_length())
+        self._index: int | None = None
 
     def get_length(self) -> int:
         return 1
@@ -20,19 +20,19 @@ class ParamTypeBoolean(ParamType[bool]):
         return struct.pack('<?', data)
 
     def reset(self) -> None:
-        self.__index = 0
+        self._index = 0
 
     def add_byte(self, byte: Byte) -> None:
         if self.is_full():
             return
-        self.__raw_data.append(byte)
-        self.__index += 1
+        self._raw_data.append(byte)
+        self._index += 1
 
     def is_full(self) -> bool:
-        return self.__index >= self.get_length()
+        return self._index >= self.get_length()
 
     def get_data(self) -> bool:
-        return self.__raw_data[0] == 1
+        return self._raw_data[0] == 1
 
     def dispose(self) -> None:
-        self.__raw_data = None
+        self._raw_data = None
